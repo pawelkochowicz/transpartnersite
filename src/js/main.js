018deb94dc2a5ceb6263b78e696137ece7ca355f
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2020-06-09 21:02:18 +0200

    feat: add media queries to navigation (not finished)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 "use strict";
 
-console.log(`Hello world!`);
+const navigationSwitcher = document.querySelector('.nav__switcher--js')
+navigationSwitcher.addEventListener('click',(e) => {
+    const navigationList = document.querySelector('.nav__list--js')
+    navigationList.classList.toggle('nav__list--visible')
+});
 
 
